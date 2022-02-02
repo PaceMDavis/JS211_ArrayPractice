a2c7748b1501d8f27cf7b21a270e7d34d7dbfa29
Pace Davis <pacemdavis@gmail.com>
2022-02-01 18:19:56 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let cars = ['chevy', 'ford', 'lincoln', 'fiat']
+let moreCars = ['pizza', 'renault', 'harley', 'honda']
+let totalCars = cars.concat(moreCars)
+let stringOfCars = totalCars.join('')
+let carsFromString = stringOfCars.split('')
+// let arrLen = function() {return arr.length};
+
+// let arrLen = () => arr.length
+
+console.log(carsFromString)
